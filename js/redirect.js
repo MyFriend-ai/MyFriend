e316c2d54c5612e8630c0fad5a996d9f30387e35
MyFriend <falamyfriend.ai@gmail.com>
2025-01-13 18:18:44 -0300

    Correcao Bugs Redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,18 +1,23 @@
 fetch('/redirects.json')
   .then(response => response.json())
   .then(data => {
-    const path = window.location.pathname.substring(1); // Remove a barra inicial
-    const shortUrl = path.split('/')[0]; // Pega a primeira parte da URL após a barra
+    const fullUrl = window.location.href; // Pega a URL completa
+    const baseUrl = window.location.origin; // Pega o domínio base (ex: https://meusite.com)
+    const path = fullUrl.substring(baseUrl.length + 1); // Extrai a parte da URL depois do domínio base
+    const shortUrl = path.split('/')[0]; // Pega a primeira parte da URL após o domínio base
 
     const link = data.links.find(item => item.shortUrl === shortUrl);
-
-    if (link) {
-      window.location.href = link.targetUrl;
-    } else {
-       // Se não encontrar um link, redireciona para a página inicial
-      window.location.href = '/';
-      // Ou você pode mostrar uma mensagem de erro
-       // alert('Link não encontrado. Você será redirecionado para a página inicial.');
+    
+    // Adicionamos essa verificação:
+    if (fullUrl === baseUrl + '/' + shortUrl) {
+      if (link) {
+          window.location.href = link.targetUrl;
+      } else {
+        // Se não encontrar um link, redireciona para a página inicial
+        window.location.href = '/';
+        // Ou você pode mostrar uma mensagem de erro
+        // alert('Link não encontrado. Você será redirecionado para a página inicial.');
+      }
     }
   })
   .catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
