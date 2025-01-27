22f3414d82c1303a3529a87489b8c956d67f656b
MyFriend <falamyfriend.ai@gmail.com>
2025-01-27 11:56:46 -0300

    Update track-clicks.js

--- js/track-clicks.js
+++ js/track-clicks.js
@@ -11,6 +11,7 @@ function generateUUID() {
     localStorage.setItem('user_id', userId);
   }
 
+
 document.addEventListener('click', function(event) {
   let destino;
   let tipo_click;
@@ -28,67 +29,61 @@ document.addEventListener('click', function(event) {
   } else if (event.target.tagName === 'BUTTON') {
        if (event.target.onclick) {
             const onclickString = event.target.onclick.toString();
-            const match = onclickString.match(/scrollIntoView\({behavior: 'smooth'}\)\'/);
-              if (match) {
-              const idMatch = onclickString.match(/document\.getElementById\('([^']+)'\)/);
-               if (idMatch) {
-                   destino = "#" + idMatch[1];
-                   tipo_click = "botao_scroll";
-                     // Preserve UTMs in scroll links
+           const match = onclickString.match(/scrollIntoView\({behavior: 'smooth'}\)\'/);
+             if (match) {
+               const idMatch = onclickString.match(/document\.getElementById\('([^']+)'\)/);
+                  if (idMatch) {
+                    destino = "#" + idMatch[1];
+                    tipo_click = "botao_scroll";
+                      // Preserve UTMs in scroll links
                      const urlParams = new URLSearchParams(window.location.search);
                      const utmParams = new URLSearchParams();
                       urlParams.forEach((value, key) => {
-                       if (key.startsWith('utm_')) {
-                          utmParams.set(key, value);
-                       }
-                   });
-                   if (utmParams.toString()) {
-                       destino += (destino.includes('?') ? '&' : '?') + utmParams.toString();
-                   }
-               }else{
-                    tipo_click = "botao_sem_destino";
-                    destino = "NA";
+                          if (key.startsWith('utm_')) {
+                              utmParams.set(key, value);
+                          }
+                     });
+                     if (utmParams.toString()) {
+                      destino += (destino.includes('?') ? '&' : '?') + utmParams.toString();
+                     }
+                   }else{
+                     tipo_click = "botao_sem_destino";
+                     destino = "NA";
                  }
-
-           } else if (event.target.onclick.toString().includes("window.location.href='")) {
-               const hrefMatch = event.target.onclick.toString().match(/window\.location\.href='(.*?)'/);
-              if (hrefMatch) {
-                   destino = hrefMatch[1];
-                  if (checkoutPlatforms.some(platform => destino.includes(platform))) {
-                     tipo_click = "INICIATE_CHECKOUT";
-                 } else{
-                    tipo_click = "botao_sem_destino";
-                 }
-             } else{
+            } else if (event.target.onclick.toString().includes("window.location.href='")) {
+                const hrefMatch = event.target.onclick.toString().match(/window\.location\.href='(.*?)'/);
+                  if (hrefMatch) {
+                    destino = hrefMatch[1];
+                    if (checkoutPlatforms.some(platform => destino.includes(platform))) {
+                         tipo_click = "INICIATE_CHECKOUT";
+                        } else{
+                           tipo_click = "botao_sem_destino";
+                          }
+                    }else{
+                        destino = "NA";
+                        tipo_click = "botao_sem_destino";
+                      }
+            }   else {
                   destino = "NA";
-                   tipo_click = "botao_sem_destino";
-                 }
-
-           } else{
-               destino = "NA";
+                  tipo_click = "botao_sem_destino";
+               }
+          }else{
+              destino = "NA";
                tipo_click = "botao_sem_destino";
-              }
-       }else{
-          destino = "NA";
-          tipo_click = "botao_sem_destino";
-         }
-
+            }
   }else{
-     return;
+    return;
   }
-
 
   const now = new Date();
   const origem = window.location.href;
   const utms = {};
-
-  const urlParamsOrigem = new URLSearchParams(window.location.search);
-     for (const [key, value] of urlParamsOrigem.entries()) {
-      if (key.startsWith('utm_')) {
-         utms[key] = value;
-       }
-    }
-
+    const urlParamsOrigem = new URLSearchParams(window.location.search);
+      for (const [key, value] of urlParamsOrigem.entries()) {
+        if (key.startsWith('utm_')) {
+             utms[key] = value;
+           }
+      }
 
   const userId = localStorage.getItem('user_id');
 
@@ -101,27 +96,18 @@ document.addEventListener('click', function(event) {
           tipo_click: tipo_click,
       };
       
- const webhookURL = 'https://my-friend-n8n.uha4jw.easypanel.host/webhook-test/03237c00-2742-47ad-af61-f561d78d24e1';
+  const webhookURL = 'https://my-friend-n8n.uha4jw.easypanel.host/webhook-test/03237c00-2742-47ad-af61-f561d78d24e1';
 
-  if (event.target.tagName === 'A' && event.target.hostname !== window.location.hostname) {
-        event.preventDefault();
-            fetch(webhookURL, {
-               method: 'POST',
-               headers: {
-                   'Content-Type': 'application/json',
-                },
-               body: JSON.stringify(data),
-              })
-              .then(() => {
-                  window.location.href = destino;
-                  });
-    } else if(event.target.tagName === 'A' || event.target.tagName === 'BUTTON') {
-         fetch(webhookURL, {
-             method: 'POST',
-             headers: {
-               'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-                });
-        }
+  fetch(webhookURL, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+      });
+
+   if (event.target.tagName === 'A' && event.target.hostname !== window.location.hostname) {
+       event.preventDefault();
+       window.location.href = destino;
+      }
 });
